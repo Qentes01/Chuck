4791a887f5f8953783b5041547102cfb71aa9eed
Opilane TTHK <opilane@tthk.ee>
2022-01-12 10:39:33 +0200

    Chuck

---
+++ ChuckConsole/HttpWebrequest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+
+namespace ChuckConsole
+{
+    internal class HttpWebrequest
+    {
+        internal string Method;
+
+        internal object GetResponce()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static explicit operator HttpWebrequest(WebRequest v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal object GetResponse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
